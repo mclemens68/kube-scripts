cae2e2431eda7e973e006becffb09a8cc700d636
Ubuntu <ubuntu@ip-192-168-40-18.ec2.internal>
2024-10-13 16:55:03 +0000

    Updates

--- k8s-ubuntu/ubuntu-22.04-init.sh
+++ k8s-ubuntu/ubuntu-22.04-init.sh
@@ -5,11 +5,6 @@ sudo apt -y install vim git netcat chrony
 # Set time zone
 sudo timedatectl set-timezone America/Chicago
 
-# Append the following to /boot/cmdline.txt
-sudo cp /boot/cmdline.txt .
-sudo sh -c 'echo " group_enable=cpuset cgroup_enable=memory cgroup_memory=1" >> cmdline.txt'
-sudo mv cmdline.txt /boot/cmdline.txt
-
 #Disable swap permanently
 sudo sync
 sudo swapoff -a
@@ -42,9 +37,9 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 sudo apt update
 sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
@@ -59,11 +54,11 @@ sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/con
 sudo systemctl restart containerd
 sudo systemctl enable containerd
 
-# Install Kubernetes (v1.28)
+# Install Kubernetes (v1.29)
 sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
 
 sudo apt update
 sudo apt install kubelet kubeadm kubectl -y
