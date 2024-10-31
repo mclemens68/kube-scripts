5ab1783960dfcbec48788bf414e683ed508c08f7
Ubuntu <ubuntu@k8s-mstr-u.clemenslabs.com>
2024-10-30 20:10:49 -0500

    updates

--- k8s-ubuntu/alma9-aws/05-label-nodes.sh
+++ k8s-ubuntu/alma9-aws/05-label-nodes.sh
@@ -1,7 +1,7 @@
 #kubectl label nodes k8s-mstr-a kubernetes.io/role=worker  
-kubectl label nodes k8s-wk1-a kubernetes.io/role=worker  
-kubectl label nodes k8s-wk2-a kubernetes.io/role=worker  
+kubectl label nodes k8s-wk1-a.clemenslabs.com kubernetes.io/role=worker  
+kubectl label nodes k8s-wk2-a.clemenslabs.com  kubernetes.io/role=worker  
 
 #kubectl label nodes k8s-mstr-a node-type=worker
-kubectl label nodes k8s-wk1-a node-type=worker
-kubectl label nodes k8s-wk2-a node-type=worker
+kubectl label nodes k8s-wk1-a.clemenslabs.com node-type=worker
+kubectl label nodes k8s-wk2-a.clemenslabs.com node-type=worker
--- k8s-ubuntu/ubuntu-22.04-aws/05-label-nodes.sh
+++ k8s-ubuntu/ubuntu-22.04-aws/05-label-nodes.sh
@@ -1,7 +1,7 @@
 #kubectl label nodes k8s-mstr-u kubernetes.io/role=worker  
-kubectl label nodes k8s-wk1-u kubernetes.io/role=worker  
-kubectl label nodes k8s-wk2-u kubernetes.io/role=worker  
+kubectl label nodes k8s-wk1-u.clemenslabs.com kubernetes.io/role=worker  
+kubectl label nodes k8s-wk2-u.clemenslabs.com kubernetes.io/role=worker  
 
 #kubectl label nodes k8s-mstr-u node-type=worker
-kubectl label nodes k8s-wk1-u node-type=worker
-kubectl label nodes k8s-wk2-u node-type=worker
+kubectl label nodes k8s-wk1-u.clemenslabs.com node-type=worker
+kubectl label nodes k8s-wk2-u.clemenslabs.com node-type=worker
