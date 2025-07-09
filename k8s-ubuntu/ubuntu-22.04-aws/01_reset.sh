a09eab964d56b57eb6c58d1b3c9ad09b1068102a
Matt Clemens <mclemens68@hotmail.com>
2025-07-09 00:24:16 -0500

    updates

--- k8s-ubuntu/ubuntu-22.04-aws/01_reset.sh
+++ k8s-ubuntu/ubuntu-22.04-aws/01_reset.sh
@@ -10,9 +10,9 @@ sudo iptables -t mangle -F
 sudo iptables -X
 sudo nft flush ruleset
 # Reset and reboot nodes
-ssh -t ubuntu@k8s-wk1-u-priv.clemenslabs.com '/home/ubuntu/reset.sh'
-ssh -t ubuntu@k8s-wk2-u-priv.clemenslabs.com '/home/ubuntu/reset.sh'
-ssh -t ubuntu@k8s-wk1-u-priv.clemenslabs.com 'sudo reboot'
-ssh -t ubuntu@k8s-wk2-u-priv.clemenslabs.com 'sudo reboot'
+ssh -t ubuntu@k8s-wk1-priv.clemenslabs.com '/home/ubuntu/reset.sh'
+ssh -t ubuntu@k8s-wk2-priv.clemenslabs.com '/home/ubuntu/reset.sh'
+ssh -t ubuntu@k8s-wk1-priv.clemenslabs.com 'sudo reboot'
+ssh -t ubuntu@k8s-wk2-priv.clemenslabs.com 'sudo reboot'
 # Reboot control node
 sudo reboot
--- k8s-ubuntu/ubuntu-22.04-aws/04-join.sh
+++ k8s-ubuntu/ubuntu-22.04-aws/04-join.sh
@@ -2,5 +2,5 @@
 kubeadm token create --print-join-command > join_cmd.txt
 join_cmd=$(<join_cmd.txt)
 
-ssh -t ubuntu@k8s-wk1-u-priv.clemenslabs.com 'sudo '$join_cmd
-ssh -t ubuntu@k8s-wk2-u-priv.clemenslabs.com 'sudo '$join_cmd
+ssh -t ubuntu@k8s-wk1-priv.clemenslabs.com 'sudo '$join_cmd
+ssh -t ubuntu@k8s-wk2-priv.clemenslabs.com 'sudo '$join_cmd
