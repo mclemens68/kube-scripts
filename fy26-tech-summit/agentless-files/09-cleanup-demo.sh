55db37645a146210111f819e9bff78af9fa3b378
Matt Clemens <mclemens68@hotmail.com>
2025-08-19 19:50:57 -0500

    Bug fixes

--- fy26-tech-summit/agentless-files/09-cleanup-demo.sh
+++ fy26-tech-summit/agentless-files/09-cleanup-demo.sh
@@ -4,10 +4,10 @@ echo "Deleting Agentless Demo resources..."
 kubectl delete -f agentless-demo.yaml
 
 echo "Deleting Bank of Anthos microservices..."
-kubectl delete -f ./bank-of-anthos/kubernetes-manifests
+kubectl delete -f ./bank-of-anthos/kubernetes-manifests -n bank-of-anthos
 
 echo "Deleting Bank of Anthos JWT secret..."
-kubectl delete -f ./bank-of-anthos/extras/jwt/jwt-secret.yaml
+kubectl delete -f ./bank-of-anthos/extras/jwt/jwt-secret.yaml -n bank-of-anthos
 
 echo "🧹 Cleanup complete."
 
