f558c12d3a123f75b58903b324e24e5bc93f77bf
Matt Clemens <mclemens68@hotmail.com>
2025-08-19 20:00:04 -0500

    One more

--- k8s-ubuntu/ubuntu-22.04-aws/05-label-nodes.sh
+++ k8s-ubuntu/ubuntu-22.04-aws/05-label-nodes.sh
@@ -1,7 +1,7 @@
-#kubectl label nodes k8s-mstr-u kubernetes.io/role=worker  
-kubectl label nodes k8s-wk1-u.clemenslabs.com kubernetes.io/role=worker  
-kubectl label nodes k8s-wk2-u.clemenslabs.com kubernetes.io/role=worker  
+#kubectl label nodes k8s-mstr kubernetes.io/role=worker  
+kubectl label nodes k8s-wk1.clemenslabs.com kubernetes.io/role=worker  
+kubectl label nodes k8s-wk2.clemenslabs.com kubernetes.io/role=worker  
 
-#kubectl label nodes k8s-mstr-u node-type=worker
-kubectl label nodes k8s-wk1-u.clemenslabs.com node-type=worker
-kubectl label nodes k8s-wk2-u.clemenslabs.com node-type=worker
+#kubectl label nodes k8s-mstr node-type=worker
+kubectl label nodes k8s-wk1.clemenslabs.com node-type=worker
+kubectl label nodes k8s-wk2.clemenslabs.com node-type=worker
